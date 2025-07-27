058281d361385ba43c2f5529cd99b52d1a89652d
NicBrito <nicolas_brito_barros@hotmail.com>
2025-07-27 00:52:34 -0300

    feat(api): Setup fastify server and dev script

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,24 @@
-    console.log("Hello from @myprofile/api!");
+    import fastify from 'fastify';
+
+    // Initializes the Fastify application
+    const app = fastify({
+        logger: true, // Enables the logger so we can see requests in the terminal
+    });
+
+    // Defines a test route
+    app.get('/', async (_request, _reply) => {
+        return { hello: 'world' };
+    });
+
+    // Function to start the server
+    const start = async () => {
+        try {
+            await app.listen({ port: 3333, host: '0.0.0.0' });
+        } catch (err) {
+            app.log.error(err);
+            process.exit(1);
+        }
+    };
+
+    // Starts the server
+    start();
